942a592fe2c6893d9af6fdb27ebfb599692998c6
1raghu <32993563+1raghu@users.noreply.github.com>
2024-07-22 02:06:23 +0530

    commit
    commit deploy

---
+++ contact.js
@@ -0,0 +1,16 @@
+export const contact = {
+  name: "RAGHAVENDRA U HEGDE",
+  job: "JAVA Developer",
+  avatar: {
+    src: "",
+    alt: ""
+  },
+  phone: "7349004041",
+  email: "rhegde98@gmail.com",
+  linkedin: "",
+  skype: "",
+  codepen: "",
+  github: "",
+  intro:
+    "Hello! I’m Dat! I love creation. I want to create something that everybody can enjoy. That's why I become a developer! Until now, I have been working for 3 years, specializing in Front - End development.I love Javascript, as well as its frameworks jQuery and React.js. My experiences have taught me that understanding 'the core' is essential in programming, no matter which languages you are using. I also enjoy researching new technology.Learning is a never- ending journey, gotta keep learning right?!"
+};
---
+++ projects.js
@@ -0,0 +1,36 @@
+export const projects = [
+  {
+    name: "ground application ME44 ",
+    thumbnail: {
+      src: "",
+      alt: ""
+    },
+    description: [
+      "ground application ME44 .",
+      "My main task in this project is maintaining the website of Comico Thailand. Fixing bugs that are reported by users and the operation team."
+    ]
+  },
+  {
+    name: "Return technology",
+    thumbnail: {
+      src: "",
+      alt: ""
+    },
+    description: [
+      "Return technology ",
+      "As a merchant participating in FedEx Multi Merchant Consolidation (MMC) returns, I need to have access to data. When the items left the FSC facility.",
+      "On the other hand, I am also responsible for adding new features to improve the User Experience and make Comico Indonesia distinct from Comico Thailand."
+    ]
+  },
+  {
+    name: "QR CODE PRODUCTS APP ",
+    thumbnail: {
+      src: "",
+      alt: ""
+    },
+    description: [
+      "A Business QR Code is a QR Code solution that creates a succinct business page for your company. ",
+      "It has a web version and an Android version. I worked in a team of 3 people to develop the Android version of Cheer Up KPI, using Android Studio."
+    ]
+  }
+];
